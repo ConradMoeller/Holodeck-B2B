74e910a21f46578af6adf4ec77240a2542475162
Conrad Moeller <moeller@bi-web.de>
2021-04-22 20:24:57 +0200

    build fat spring-boot jar / add to distribution

---
+++ modules/holodeckb2b-distribution/basedir/bin/startWebUI.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# ----------------------------------------------------------------------------
+# Holodeck B2B web UI start script
+#
+# Environment Variable Prequisites
+#
+#   H2B_HOME   Home of the Holodeck B2B installation. If not set I will try
+#              to figure it out.
+#
+#   JAVA_HOME  Must point at your Java Runtime Environment
+#
+# -----------------------------------------------------------------------------
+
+# Get the context and from that find the location of setenv.sh
+. `dirname $0`/setenv.sh
+
+cd "$HB2B_HOME"
+
+exec "$JAVA_HOME/bin/java" -jar ./lib/holodeckb2b-webui.jar
